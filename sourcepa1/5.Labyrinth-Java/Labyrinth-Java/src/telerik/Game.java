d7c08dcd85f18cb716fe8823c49b6627945e09d5
Julian Jark <juliuxu@gmail.com>
2016-03-10 14:12:51 +0100

    comment Game.java and remove isExit

--- sourcepa1/5.Labyrinth-Java/Labyrinth-Java/src/telerik/Game.java
+++ sourcepa1/5.Labyrinth-Java/Labyrinth-Java/src/telerik/Game.java
@@ -3,27 +3,25 @@ package telerik;
 import java.util.Scanner;
 
 
-
 public class Game {
 	public static void main(String[] args){
 		generirane labyrinth = new generirane();
 		labyrinth.initializeScoreBoard();
+
 		while(true){
+
+			// Take commands till the player has solved the maze
 			labyrinth.initializeMaze();
 			while((labyrinth.playersCurrentColumn!=0)&&(labyrinth.playersCurrentColumn!=6)
 					&&(labyrinth.playersCurrentRow!=0)&&(labyrinth.playersCurrentRow!=6)){
 				labyrinth.inputCommand();
-				
-				
-				
 			}
 			System.out.println();
 			labyrinth.printMaze();
 			System.out.println("Congratulations! You escaped in "+labyrinth.playersMovesCount+" moves.");
 			System.out.println();
 			
-			
-			
+			// Take the player name and add to the scoreboard
 			Scanner scanner = new Scanner(System.in);
 			System.out.println("Enter your name : ");
 			String name = scanner.next();
@@ -32,7 +30,8 @@ public class Game {
 				System.out.println("Your score is in top 5!");
 				labyrinth.board.printBoard(labyrinth.board.list);
 			}
-			labyrinth.isExit = false;
+
+			// Reset the maze
 			labyrinth.playersCurrentColumn = 3;
 			labyrinth.playersCurrentRow = 3;
 			labyrinth.playersMovesCount = 0;
